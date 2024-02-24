483b91a92a12f94d98e7690a85ee5d4b905ab8df
Rakbow <rakbow@gmail.com>
2024-02-25 01:51:58 +0800

    引入EF Core模块

---
+++ KureaKurusu.Web/Access/ApplicationDbContext.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.Json;
+using KureaKurusu.Data;
+using KureaKurusu.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace KureaKurusu.Web.Access;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+    public DbSet<Person> Persons { get; set; } = null!;
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .Entity<Person>().ToTable("person")
+            .Property(e => e.Aliases)
+            .HasConversion(
+                v => JsonSerializer.Serialize(v, (JsonSerializerOptions)null!),
+                v => JsonSerializer.Deserialize<List<string>>(v, (JsonSerializerOptions)null!)
+            );
+        
+        modelBuilder
+            .Entity<Person>().ToTable("person")
+            .Property(e => e.Links)
+            .HasConversion(
+                v => JsonSerializer.Serialize(v, (JsonSerializerOptions)null!),
+                v => JsonSerializer.Deserialize<List<Link>>(v, (JsonSerializerOptions)null!)
+            );
+
+        // 其他配置...
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
