578452ffe44c04461572fcec1f12b84aa71a1493
Rakbow <rakbow@gmail.com>
2024-02-25 01:51:34 +0800

    配置启动项

--- KureaKurusu.Web/Program.cs
+++ KureaKurusu.Web/Program.cs
@@ -1,6 +1,48 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using KureaKurusu.Web;
+using KureaKurusu.Web.Access;
+using KureaKurusu.Web.Service;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
-app.MapGet("/", () => "Hello World!");
+[assembly: ApiController]
+var builder = WebApplication.CreateBuilder(args);
+
+// var startup = new Startup(builder.Configuration);
+// startup.ConfigureServices(builder.Services);
+        
+builder.Services.AddDbContext<ApplicationDbContext>(
+    options => options.UseMySQL(builder.Configuration.GetConnectionString("RakbowConnection")!));
+
+builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// 注册自定义服务
+builder.Services.AddSingleton<PersonService>();
+
+var app = builder.Build();
+// startup.Configure(app, builder.Environment);
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
 
 app.Run();
